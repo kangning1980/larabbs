e99ddff7b35ac92aada89c47e3f6b54b9bb0de5b
kangning1980 <ka.1980@163.com>
2019-03-17 10:00:58 +0800

    WYSIWYG 编辑器

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,8 @@
     <!-- Styles -->
     <link href="{{ url('/').mix('css/app.css') }}" rel="stylesheet">
 
+    @yield('styles')
+
 </head>
 
 <body>
@@ -34,6 +36,9 @@
 
 <!-- Scripts -->
 <script src="{{ url('/').mix('js/app.js') }}"></script>
+
+@yield('scripts')
+
 </body>
 
 </html>
